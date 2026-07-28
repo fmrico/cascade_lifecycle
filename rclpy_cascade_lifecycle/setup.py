2fa42e31dff95ca8958ce944205de55b2ab3bf2e
Francisco Martín Rico <fmrico@gmail.com>
2026-07-28 11:16:23 +0200

    2.0.5

--- rclpy_cascade_lifecycle/setup.py
+++ rclpy_cascade_lifecycle/setup.py
@@ -6,7 +6,7 @@ package_name = 'rclpy_cascade_lifecycle'
 
 setup(
     name=package_name,
-    version='2.0.4',
+    version='2.0.5',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
